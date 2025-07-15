f88d4bae533e0b379d9db4bcd3ca8d3e46c34f6d
k-aniket-01 <khomaneaniket9420@gmail.com>
2025-07-15 17:29:40 +0530

    allowed host *

--- diarybackend/settings.py
+++ diarybackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#x6#so)mm9egy5&c_12zv#4ad84j%at2g-1gy*l)osy#sz^4_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
